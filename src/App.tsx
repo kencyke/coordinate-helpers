fc5da463eb3e8f67ff321fa7a0810ba5c120a755
kencyke <suzuki_keisuke@kurusugawa.jp>
2021-05-01 09:48:25 +0900

    spin box

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,41 @@
-import React from "react"
-import logo from "./logo.svg"
+import React, { useRef } from "react"
+import { Color3, Mesh, Nullable, Vector3 } from "@babylonjs/core"
+import { Engine, Scene, useBeforeRender } from "react-babylonjs"
 import "./App.css"
 
-function App() {
+type SpinningBoxProps = {
+  name: string
+  position: Vector3
+  color: Color3
+}
+
+const SpinningBox: React.VFC<SpinningBoxProps> = (props) => {
+  const boxRef = useRef<Nullable<Mesh>>(null)
+
+  const rpm = 5
+  useBeforeRender((scene) => {
+    if (boxRef.current) {
+      const deltaTimeInMillis = scene.getEngine().getDeltaTime()
+      boxRef.current.rotation.y += (rpm / 60) * Math.PI * 2 * (deltaTimeInMillis / 1000)
+    }
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <box name={props.name} ref={boxRef} size={2} position={props.position}>
+      <standardMaterial name={`${props.name}-mat`} diffuseColor={props.color} specularColor={Color3.Black()} />
+    </box>
+  )
+}
+
+const App: React.VFC = () => {
+  return (
+    <Engine antialias adaptToDeviceRatio canvasId="renderCanvas">
+      <Scene>
+        <arcRotateCamera name="camera1" target={Vector3.Zero()} alpha={Math.PI / 2} beta={Math.PI / 4} radius={8} />
+        <hemisphericLight name="light1" intensity={0.7} direction={Vector3.Up()} />
+        <SpinningBox name="box1" position={Vector3.Zero()} color={Color3.FromHexString("#C8F4F9")} />
+      </Scene>
+    </Engine>
   )
 }
 
